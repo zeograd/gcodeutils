0aed74bc9b4481f45da7dc642750380b8ba8b386
Olivier Jolly <olivier@pcedev.com>
2015-07-14 11:55:57 +0200

    bumped version to 1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9.9',
+    version='1.0',
 
     description='Reprap oriented gcode utilities',
     long_description=long_description,
