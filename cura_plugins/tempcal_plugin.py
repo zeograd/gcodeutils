d0b0d3b250643be1907abfdebbaaa6d8fdc35251
Olivier Jolly <olivier@pcedev.com>
2015-07-27 10:07:29 +0200

    Added cura plugin for temperature tower calibration

---
+++ cura_plugins/tempcal_plugin.py
@@ -0,0 +1,18 @@
+#Name: Temp tower
+#Info: Update temperature continuously
+#Depend: GCode
+#Type: postprocess
+#Param: start_temp(float:150) Temperature at bottom of calibration object
+#Param: end_temp(float:240) Temperature at top of calibration object
+#Param: min_z_change(float:3) Minimum Z for changing temperatures
+#Param: steps(float:10) Number of steps
+
+from gcodeutils.gcode_tempcal import GCodeStepTempGradient
+from gcodeutils.gcoder import GCode
+
+with open(filename, 'r') as gcode_file:  # pylint: disable=undefined-variable
+    gcode = GCode(gcode_file)  # pylint: disable=redefined-outer-name,invalid-name
+
+with open(filename, 'w') as gcode_file:  # pylint: disable=undefined-variable
+    GCodeStepTempGradient(gcode=gcode, start_temp=start_temp, end_temp=end_temp, min_z_change=min_z_change,
+                          steps=steps).write(gcode_file)
