c8333ed6e4c6bdc27275a59bdcd770755462ae7b
Eyck Jentzsch <eyck@minres.com>
2016-06-05 19:16:55 +0200

    Fixed formatting

--- gcodeutils/filter/arc_optimizer.py
+++ gcodeutils/filter/arc_optimizer.py
@@ -1,3 +1,11 @@
+# GCodeUtils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GCodeUtils.  If not, see <http://www.gnu.org/licenses/>.
+
 #import logging
 from math import sqrt
 import cmath
@@ -8,7 +16,7 @@ __author__ = 'Eyck Jentzsch <eyck@jepemuc.de>'
 
 MIN_SEGMENTS = 8
 ALIGNMENT_ERROR = 0.001  # mm
-EXTRUSION_ERROR = 0.15   # percent
+EXTRUSION_ERROR = 0.15  # percent
 
 
 class Point(object):
@@ -29,12 +37,12 @@ class Point(object):
         return Point(self.x - other.x, self.y - other.y)
 
     def __str__(self):
-        return "Point(" + self.x + "," + self.y + ")"
+        return "Point(%f, %f)" % self.x, self.y
 
     def amplitude(self):
         return sqrt(self.x * self.x + self.y * self.y)
 
-    def distanceTo(self, other):
+    def distance_to(self, other):
         xdiff = self.x - other.x
         xdiffsq = pow(xdiff, 2)
         ydiff = self.y - other.y
@@ -57,7 +65,8 @@ class Circle(object):
         self.end = end
 
     def __str__(self):
-        return self.direction+"-Cicrle(r=" + self.radius + ",center" + self.center + "from "+self.start+" to "+self.end+")"
+        return self.direction + "-Cicrle(r=%f, center=%s from %s to %s" % self.radius, self.center.__str__(), \
+               self.start.__str__(), self.end.__str__()
 
 
 class GCodeArcOptimizerFilter(GCodeFilter):
@@ -65,65 +74,55 @@ class GCodeArcOptimizerFilter(GCodeFilter):
 
     queue = []
     valid_circle = False
-    alignment_error = 0.0001
-    last_circle = Circle()
-    extrusion_error = 0.15
-    pos_error = 0.1
-
-    def __init__(self, x=None, y=None, **kwargs):
-        self.translate_x = x or 0.
-        self.translate_y = y or 0.
-        self.first_move_after_home = False
-        self.absolute_distance_mode = None  # None if when it is unknown
 
     def opcode_filter(self, opcode):
         return opcode
 
-    def getCircleLeastSquares(self):
-        N = len(self.queue)
-        xbar = sum(i.x for i in self.queue) / N
-        ybar = sum(i.y for i in self.queue) / N
-        Suu = 0
-        Suuu = 0
-        Suvv = 0
-        Suv = 0
-        Svv = 0
-        Svvv = 0
-        Svuu = 0
+    def get_circle_least_squares(self):
+        count = len(self.queue)
+        xbar = sum(i.current_x for i in self.queue) / count
+        ybar = sum(i.current_y for i in self.queue) / count
+        suu = 0
+        suuu = 0
+        suvv = 0
+        suv = 0
+        svv = 0
+        svvv = 0
+        svuu = 0
         for line in self.queue:
-            pt = Point(line.x - xbar, line.y - ybar)
-            Suu += pt.x * pt.x
-            Suuu += pt.x * pt.x * pt.x
-            Suvv += pt.x * pt.y * pt.y
-            Suv += pt.x * pt.y
-            Svv += pt.y * pt.y
-            Svvv += pt.y * pt.y * pt.y
-            Svuu += pt.y * pt.x * pt.x
-        v = (((Svvv + Svuu) / 2) - ((Suv / 2) * ((Suuu + Suvv) / Suu))) / (((-(Suv * Suv)) / Suu) + Svv)
-        u = (((Suuu + Suvv) / 2) - (v * Suv)) / (Suu)
+            pt = Point(line.current_x - xbar, line.current_y - ybar)
+            suu += pt.x * pt.x
+            suuu += pt.x * pt.x * pt.x
+            suvv += pt.x * pt.y * pt.y
+            suv += pt.x * pt.y
+            svv += pt.y * pt.y
+            svvv += pt.y * pt.y * pt.y
+            svuu += pt.y * pt.x * pt.x
+        v = (((svvv + svuu) / 2) - ((suv / 2) * ((suuu + suvv) / suu))) / (((-(suv * suv)) / suu) + svv)
+        u = (((suuu + suvv) / 2) - (v * suv)) / suu
         # calculate direction
         center = Point(x=u + xbar, y=v + ybar)
-        p0 = cmath.phase(complex(self.queue[0].x - center.x, self.queue[0].y - center.y))
-        p1 = cmath.phase(complex(self.queue[1].x - center.x, self.queue[1].y - center.y))
-        return Circle(radius=sqrt((u * u) + (v * v) + ((Suu + Svv) / N)),
-                         center=center,
-                         direction="CCW" if p0 < p1 else "CW",
-                         start=Point(self.queue[0].x, self.queue[0].y),
-                         end=Point(self.queue[-1].x, self.queue[-1].y))
+        p0 = cmath.phase(complex(self.queue[0].current_x - center.x, self.queue[0].current_y - center.y))
+        p1 = cmath.phase(complex(self.queue[1].current_x - center.x, self.queue[1].current_y - center.y))
+        return Circle(radius=sqrt((u * u) + (v * v) + ((suu + svv) / count)),
+                      center=center,
+                      direction="CCW" if p0 < p1 else "CW",
+                      start=Point(self.queue[0].current_x, self.queue[0].current_y),
+                      end=Point(self.queue[-1].current_x, self.queue[-1].current_y))
 
-    def getCircleErrors(self, circle):
+    def get_circle_errors(self, circle):
         errors = []
         for line in self.queue:
-            length = Point(circle.center.x - line.x, circle.center.y - line.y).amplitude()
+            length = Point(circle.center.x - line.current_x, circle.center.y - line.current_y).amplitude()
             errors.append(abs(length - circle.radius))
         return errors
 
-    def getCircle(self):
-        circle = self.getCircleLeastSquares()
-        errors = self.getCircleErrors(circle)
+    def get_circle(self):
+        circle = self.get_circle_least_squares()
+        errors = self.get_circle_errors(circle)
         return any([error > ALIGNMENT_ERROR for error in errors]), circle
 
-    def getDistances(self):
+    def get_distances(self):
         extrusions = {
             'total': {
                 'path': 0,
@@ -138,15 +137,16 @@ class GCodeArcOptimizerFilter(GCodeFilter):
             'path': {},
             'ratio': {}
         }
-        prev = None;
+        prev = None
         for num, line in enumerate(self.queue):
             if prev is not None:
-                path = round(Point(line.current_x, line.current_y).distanceTo(Point(prev.current_x, prev.current_y)), 7)
-                filLength = 0 if prev.e is None else line.e if line.relative_e else (line.e - prev.e)
-                extrusions['total']['filament'] += filLength
-                extrusions['total']['path'] += path;
-                extrusions['filament'][num] = filLength
-                extrusions['path'][num] = path;
+                path = round(Point(line.current_x, line.current_y).distance_to(
+                    Point(prev.current_x, prev.current_y)), 7)
+                extrusion = line.e if line.relative_e else (line.e - (0 if prev.e is None else prev.e))
+                extrusions['total']['filament'] += extrusion
+                extrusions['total']['path'] += path
+                extrusions['filament'][num] = extrusion
+                extrusions['path'][num] = path
                 extrusions['ratio'][num] = extrusions['filament'][num] / path
             prev = line
         extrusions['avg']['filament'] = extrusions['total']['filament'] / len(self.queue)
@@ -154,31 +154,32 @@ class GCodeArcOptimizerFilter(GCodeFilter):
         extrusions['avg']['ratio'] = extrusions['total']['filament'] / extrusions['total']['path']
         return extrusions
 
-    def queueValid(self):
-        ## check from 1 as idx 0 indicates the move to the start point
-        allE = False if self.queue[1].e is None else True
-        allF = False if self.queue[1].f is None else True
-        curZ = self.queue[1].current_z
+    def queue_valid(self):
+        # check from 1 as idx 0 indicates the move to the start point
+        all_e = False if self.queue[1].e is None else True
+        all_f = False if self.queue[1].f is None else True
+        cur_z = self.queue[1].current_z
         for line in self.queue[1:]:
-            allE &= line.f is None and line.e is not None
-            allF &= line.e is None and line.f is not None
-            if curZ != line.current_z: # check is all in same layer
+            all_e &= line.f is None and line.e is not None
+            all_f &= line.e is None and line.f is not None
+            if cur_z != line.current_z:  # check is all in same layer
                 return True, None
-        if not (allE or allF):
+        if not (all_e or all_f):
             return True, None
-        if allE:
-            extrusions = self.getDistances()
+        if all_e:
+            extrusions = self.get_distances()
             valid = all(abs((curext / extrusions['avg']['ratio']) - 1) < EXTRUSION_ERROR for curext in
                         extrusions['ratio'].itervalues())
             if not valid:
                 return True, None
-        return self.getCircle()
+        return self.get_circle()
 
-    def toGcode(self, last=None):
+    def to_gcode(self):
         first = self.queue[0]
         last = self.queue.pop()
-        error, circle = self.getCircle()
-        extrusions = self.getDistances()
+        count = len(self.queue)
+        error, circle = self.get_circle()
+        extrusions = self.get_distances()
         op1 = Line()
         op1.command = "G3" if circle.direction == "CCW" else "G2"
         op1.x = round(circle.end.x, 3)
@@ -187,36 +188,37 @@ class GCodeArcOptimizerFilter(GCodeFilter):
         op1.j = round(circle.center.y - circle.start.y, 3)
         op1.e = extrusions['total']['filament']
         unsplit(op1)
+        op1.raw += "; generated from %s segments" % (count-1)
         self.valid_circle = False
         return first, op1, last
 
-    def opcode_filter(self, opcode):
-        self.queue.append(opcode)
+    def opcode_filter(self, x):
+        self.queue.append(x)
         count = len(self.queue)
         if count > MIN_SEGMENTS:
             if not self.queue[-1].command in move_gcodes:
                 if self.valid_circle:
                     # the last element inserted invalidated the circle, so process & flush
-                    result = self.toGcode()
+                    result = self.to_gcode()
                 else:
                     # flush the queue as we have a GCode resetting the processing
                     result = self.queue[:]
                 self.queue = []
                 return result
             else:
-                error, circle = self.queueValid()
+                error, circle = self.queue_valid()
                 if error:
                     if self.valid_circle:
                         # the last element inserted invalidated the circle
-                        first, circleGcode, last = self.toGcode()
+                        first, circle_gcode, last = self.to_gcode()
                         # since last elem was a move keep it in the queue
                         self.queue = [last]
-                        return [first, circleGcode]
+                        return [first, circle_gcode]
                     else:
                         return self.queue.pop(0)
                 else:
                     self.valid_circle = True
-                    return None
+                    return []
         else:
             if not self.queue[-1].command in move_gcodes:
                 # flush the queue as we have a GCode resetting the processing
@@ -225,4 +227,4 @@ class GCodeArcOptimizerFilter(GCodeFilter):
                 self.valid_circle = False
                 return result
             else:
-                return None
+                return []
--- gcodeutils/tests/test_arc_optimization.py
+++ gcodeutils/tests/test_arc_optimization.py
@@ -1,39 +1,28 @@
+# GCodeUtils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GCodeUtils.  If not, see <http://www.gnu.org/licenses/>.
+
 import logging
 
-from gcodeutils.stretch.stretch import SkeinforgeStretchFilter, Slic3rStretchFilter, CuraStretchFilter
+from gcodeutils.filter.arc_optimizer import GCodeArcOptimizerFilter
 from gcodeutils.tests import open_gcode_file, gcode_eq
+from gcodeutils.gcoder import PyLine
+__author__ = 'Eyck Jentzsch <eyck@jepemuc.de>'
 
-__author__ = 'olivier'
+PyLine.EQ_EPSILON=1e-4 # write to disc truncates to 4 digits after comma so accuraccy needs to be adapted
 
+def test_arc_optimization():
+    gcode = open_gcode_file('arc_raw.gcode')
+    gcode_oracle = open_gcode_file('arc_raw-post.gcode')
 
-def test_skeinforge_formatted_stretch():
-    gcode_oracle = open_gcode_file('skeinforge_model1_poststretch.gcode')
-    gcode = open_gcode_file('skeinforge_model1_prestretch.gcode')
-
-    SkeinforgeStretchFilter().filter(gcode)
-
+    logging.basicConfig(level=logging.DEBUG)
+    GCodeArcOptimizerFilter().filter(gcode)
     gcode_eq(gcode_oracle, gcode)
 
 
-def test_square_stretch_skeinforge():
-    simple_square_gcode = open_gcode_file('skeinforge_square.gcode')
-
-    logging.basicConfig(level=logging.DEBUG)
-    SkeinforgeStretchFilter().filter(simple_square_gcode)
-    simple_square_gcode.write()
-
-
-def test_square_stretch_slic3r():
-    simple_square_gcode = open_gcode_file('slic3r_square.gcode')
-
-    logging.basicConfig(level=logging.DEBUG)
-    Slic3rStretchFilter().filter(simple_square_gcode)
-    simple_square_gcode.write()
-
-
-def test_square_stretch_cura():
-    simple_square_gcode = open_gcode_file('cura_square.gcode')
-
-    logging.basicConfig(level=logging.DEBUG)
-    CuraStretchFilter().filter(simple_square_gcode)
-    simple_square_gcode.write()
+if __name__ == "__main__":
+    test_arc_optimization()
